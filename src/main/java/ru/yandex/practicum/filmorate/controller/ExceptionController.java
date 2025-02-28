25b0e00f534499b67076d7be2e7681e88ae0cd48
Ivan G <grikoivan@yandex.ru>
2025-02-28 15:51:14 +0300

    ФЗ 11

--- src/main/java/ru/yandex/practicum/filmorate/controller/ExceptionController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ExceptionController.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
