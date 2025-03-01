b1905c074a2a5fb3f852a157a966cd4ca4c4f430
Ivan G <grikoivan@yandex.ru>
2025-03-02 00:25:21 +0300

    ФЗ 11

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -43,7 +43,7 @@ public class UserController {
         return userService.updateUser(user);
     }
 
-    @PatchMapping("/{id}/friends/{friendId}")
+    @PutMapping("/{id}/friends/{friendId}")
     public User addFriend(@PathVariable long id, @PathVariable long friendId) {
         return userService.addFriend(id, friendId);
     }
