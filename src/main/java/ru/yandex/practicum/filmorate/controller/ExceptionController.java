0163c62decb8b53177a148b6b84eb38b7304197f
Ivan G <grikoivan@yandex.ru>
2025-03-01 22:54:22 +0300

    ФЗ 11

--- src/main/java/ru/yandex/practicum/filmorate/controller/ExceptionController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ExceptionController.java
@@ -55,6 +55,7 @@ public class ExceptionController {
     @Getter
     public static class ErrorResponse {
         private final String error;
+
         public ErrorResponse(String error) {
             this.error = error;
         }
