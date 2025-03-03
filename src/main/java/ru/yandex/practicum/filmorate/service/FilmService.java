09fa68f3b5a6582b90a33e34de15cb8bed8bc9da
Ivan G <grikoivan@yandex.ru>
2025-03-03 20:45:42 +0300

    ФЗ 11

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -45,7 +45,7 @@ public class FilmService {
         return filmStorage.removeLike(filmId, userId);
     }
 
-    public Set<Film> getPopularFilms(Integer count) {
+    public Set<Film> getPopularFilms(int count) {
         return filmStorage.getPopularFilms(count);
     }
 }
