76ab807cea1d4f60808d377be4ba8579a1dea742
Ivan G <grikoivan@yandex.ru>
2025-03-04 00:29:47 +0300

    ФЗ 11

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -10,7 +11,7 @@ import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.Collection;
-import java.util.List;
+//import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -69,8 +70,11 @@ public class FilmController {
         return filmService.removeLike(filmId, userId);
     }
 
-    @GetMapping("/popular?count={count}")
-    public List<Film> getPopularFilms(@RequestParam(required = false) Integer count) {
+    @GetMapping("/popular")
+    @ResponseStatus(HttpStatus.OK)
+    public Collection<Film> getPopularFilms(@RequestParam(value = "count", defaultValue = "10") Integer count) {
+//    @GetMapping("/popular?count={count}")
+//    public List<Film> getPopularFilms(@RequestParam(required = false) Integer count) {
         if (count == null || count < 1) {
             count = 10;
         }
