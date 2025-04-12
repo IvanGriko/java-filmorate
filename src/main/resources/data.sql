848f4762f7ef616f8c1e234fd81761c06cde003e
Ivan G <grikoivan@yandex.ru>
2025-04-12 12:14:28 +0300

    Реализована БД. Добавил IF NOT EXISTS в условия уникальных имен MPA & Genres

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,101 @@
-INSERT INTO genres (name) VALUES('Комедия');
-INSERT INTO genres (name) VALUES('Драма');
-INSERT INTO genres (name) VALUES('Мультфильм');
-INSERT INTO genres (name) VALUES('Триллер');
-INSERT INTO genres (name) VALUES('Документальный');
-INSERT INTO genres (name) VALUES('Боевик');
+INSERT INTO genres (name)
+SELECT 'Комедия'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres
+    WHERE name = 'Комедия'
+);
 
-INSERT INTO mpa_ratings (name) VALUES('G');
-INSERT INTO mpa_ratings (name) VALUES('PG');
-INSERT INTO mpa_ratings (name) VALUES('PG-13');
-INSERT INTO mpa_ratings (name) VALUES('R');
-INSERT INTO mpa_ratings (name) VALUES('NC-17');
+INSERT INTO genres (name)
+SELECT 'Драма'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres
+    WHERE name = 'Драма'
+);
+
+INSERT INTO genres (name)
+SELECT 'Мультфильм'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres
+    WHERE name = 'Мультфильм'
+);
+
+INSERT INTO genres (name)
+SELECT 'Триллер'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres
+    WHERE name = 'Триллер'
+);
+
+INSERT INTO genres (name)
+SELECT 'Документальный'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres
+    WHERE name = 'Документальный'
+);
+
+INSERT INTO genres (name)
+SELECT 'Боевик'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres
+    WHERE name = 'Боевик'
+);
+
+INSERT INTO mpa_ratings (name)
+SELECT 'G'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM mpa_ratings
+    WHERE name = 'G'
+);
+
+INSERT INTO mpa_ratings (name)
+SELECT 'PG'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM mpa_ratings
+    WHERE name = 'PG'
+);
+
+INSERT INTO mpa_ratings (name)
+SELECT 'PG-13'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM mpa_ratings
+    WHERE name = 'PG-13'
+);
+
+INSERT INTO mpa_ratings (name)
+SELECT 'R'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM mpa_ratings
+    WHERE name = 'R'
+);
+
+INSERT INTO mpa_ratings (name)
+SELECT 'NC-17'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM mpa_ratings
+    WHERE name = 'NC-17'
+);
+
+
+--INSERT INTO genres (name) VALUES('Комедия');
+--INSERT INTO genres (name) VALUES('Драма');
+--INSERT INTO genres (name) VALUES('Мультфильм');
+--INSERT INTO genres (name) VALUES('Триллер');
+--INSERT INTO genres (name) VALUES('Документальный');
+--INSERT INTO genres (name) VALUES('Боевик');
+--
+--INSERT INTO mpa_ratings (name) VALUES('G');
+--INSERT INTO mpa_ratings (name) VALUES('PG');
+--INSERT INTO mpa_ratings (name) VALUES('PG-13');
+--INSERT INTO mpa_ratings (name) VALUES('R');
+--INSERT INTO mpa_ratings (name) VALUES('NC-17');
